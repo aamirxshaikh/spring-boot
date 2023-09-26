08fb6910b0ff923f5bb85f7f5ec0a8e2ca7603ce
Aamir shaikh <aamirshaikh3232@outlook.com>
2023-09-26 23:55:33 +0530

    Refactor Product Entity and Controller
    This commit introduces the following changes to the codebase:
    
    - Added new CRUD operations (POST, PUT, DELETE) to the ProductController to handle product creation, update, and deletion.
    - Updated the ProductRepositoryFake to include the `secretKey` property.
    - Refactored the Product entity class to include a `secretKey` property and used Jackson annotations (`@JsonProperty` and `@JsonIgnore`) for better JSON serialization/deserialization control.
    
    Purpose:
    The purpose of this commit is to enhance the Product entity to work seamlessly with JSON serialization and deserialization, provide more flexible CRUD operations, and ensure consistent data handling across the application.

--- src/main/java/com/example/springboot/product/Product.java
+++ src/main/java/com/example/springboot/product/Product.java
@@ -7,14 +7,16 @@ public class Product {
   private final Long id;
   private final String name;
   private final Double price;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private final String secretKey;
 
-  public Product(Long id, String name, Double price) {
+  public Product(Long id, String name, Double price, String secretKey) {
     this.id = id;
     this.name = name;
     this.price = price;
+    this.secretKey = secretKey;
   }
 
-  @JsonProperty("productId")
   public Long getId() {
     return id;
   }
@@ -23,21 +25,17 @@ public class Product {
     return name;
   }
 
-  public String getNameAgain() {
-    return name;
-  }
-
-  @JsonIgnore
   public Double getPrice() {
     return price;
   }
 
+  @JsonIgnore
+  public String getSecretKey() {
+    return secretKey;
+  }
+
   @Override
   public String toString() {
-    return "Customer{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", price=" + price +
-            '}';
+    return "Product{" + "id=" + id + ", name='" + name + '\'' + ", price=" + price + ", secretKey='" + secretKey + '\'' + '}';
   }
 }
--- src/main/java/com/example/springboot/product/ProductController.java
+++ src/main/java/com/example/springboot/product/ProductController.java
@@ -1,9 +1,7 @@
 package com.example.springboot.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,24 @@ public class ProductController {
   @GetMapping
   List<Product> getProducts() {
     return productService.getProducts();
+  }
+
+  @PostMapping
+  void createProduct(@RequestBody Product product) {
+    System.out.println("POST");
+    System.out.println(product);
+  }
+
+  @PutMapping("{id}")
+  void updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+    System.out.println("PUT");
+    System.out.println(id);
+    System.out.println(product);
+  }
+
+  @DeleteMapping("{id}")
+  void deleteProduct(@PathVariable("id") Long id) {
+    System.out.println("DELETE");
+    System.out.println(id);
   }
 }
