9692533e0767fcda1b678bbf5afe62d6e081e4f1
Aamir shaikh <aamirshaikh3232@outlook.com>
2023-12-20 22:45:24 +0530

    Remove AppConfiguration class and Update Repository Implementations
    This commit introduces the following changes to the codebase:
    
    - Removed `AppConfiguration.java` as it's no longer required as transitioning to JPA repositories.
    - Added `@Primary` annotations to `CustomerRepositoryImpl` and `ProductRepositoryImpl` classes.
    - Updated `CustomerService` and `ProductService` constructors to use `@Qualifier` annotations for dependency injection of repository beans.
    
    Purpose:
    The purpose of this commit is to streamline the bean creation process and ensure that the correct repository implementations are used throughout the application.

--- src/main/java/com/example/springboot/customer/CustomerRepositoryImpl.java
+++ src/main/java/com/example/springboot/customer/CustomerRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.example.springboot.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
 @Repository
+@Primary
 public class CustomerRepositoryImpl implements CustomerRepository {
   @Override
   public Customer getCustomer(Long id) {
--- src/main/java/com/example/springboot/product/ProductRepositoryImpl.java
+++ src/main/java/com/example/springboot/product/ProductRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.example.springboot.product;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
 @Repository
+@Primary
 public class ProductRepositoryImpl implements ProductRepository {
   @Override
   public List<Product> getProducts() {
