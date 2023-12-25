50a3814ee0312edf8dcb7151ac08c93913f1a7c8
Aamir shaikh <aamirshaikh3232@outlook.com>
2023-12-25 22:41:36 +0530

    Configure DataSource and Add additional configration for Different Environments
    This commit introduces the following changes to the codebase:
    
    - DataSourceConfiguration:
      - Added `driverClassName` property to `DataSourceConfiguration` class.
    
    - DataSourceController:
      - Updated the request mapping URL to "/api/data-source".
      - Added a `@Profile` annotation to include the controller only in "dev", "staging", and "test" profiles.
    
    - application-dev.properties:
      - Modified H2 database URL to "jdbc:h2:mem:dev".
      - Added JPA, H2 console configurations.
    
    - application-prod.properties:
      - Modified H2 database URL to "jdbc:h2:mem:prod".
      - Added JPA, H2 console configurations.
    
    - application-staging.properties:
      - Modified H2 database URL to "jdbc:h2:mem:staging".
      - Added JPA, H2 console configurations.
    
    - application-test.properties:
      - Modified H2 database URL to "jdbc:h2:mem:test".
      - Added JPA, H2 console configurations.
    
    - application.properties:
      - Set default H2 database URL to "jdbc:h2:mem:local".
      - Added default configurations (logging, database, exception handling).
    
    Purpose:
    The purpose of this commit is to enhance DataSource configuration and fine-tune other configurations for different environments, ensuring consistency and security.

--- src/main/java/com/example/springboot/db/DataSourceConfiguration.java
+++ src/main/java/com/example/springboot/db/DataSourceConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.springboot.db;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "datasource")
+@ConfigurationProperties(prefix = "spring.datasource")
 @Component
 public class DataSourceConfiguration {
   private String url;
+  private String driverClassName;
   private String username;
   private String password;
 
@@ -17,6 +17,14 @@ public class DataSourceConfiguration {
 
   public void setUrl(String url) {
     this.url = url;
+  }
+
+  public String getDriverClassName() {
+    return driverClassName;
+  }
+
+  public void setDriverClassName(String driverClassName) {
+    this.driverClassName = driverClassName;
   }
 
   public String getUsername() {
--- src/main/java/com/example/springboot/db/DataSourceController.java
+++ src/main/java/com/example/springboot/db/DataSourceController.java
@@ -1,11 +1,13 @@
 package com.example.springboot.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/datasource")
+@RequestMapping("/api/data-source")
+@Profile({"dev", "staging", "test"})
 public class DataSourceController {
   private final DataSourceConfiguration dataSourceConfiguration;
 
